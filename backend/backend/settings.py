033cf76c9d11f2931c1b9057657d2787ef99d073
hasnain sikandar <s20755753@gmail.com>
2021-10-31 13:41:58 +0500

    logs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -104,13 +104,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = '94.mbbs.2@gmail.com'
-EMAIL_HOST_PASSWORD = 'hfbuehwaolnzujut'
+EMAIL_HOST_PASSWORD = 'Asd890ml'
 EMAIL_USE_TLS = True
 
 
 DJOSER = {
     'LOGIN_FIELD': 'email',
-    'USER_CREATE_PASSWORD_RETYPE': False,
+    
     'USERNAME_CHANGED_EMAIL_CONFIRMATION': True,
     'PASSWORD_CHANGED_EMAIL_CONFIRMATION': True,
     'SEND_CONFIRMATION_EMAIL': True,
