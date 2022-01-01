fa12d6e179623e796469c7189cc5b5fb2004ab47
hasnain sikandar <s20755753@gmail.com>
2022-01-02 01:55:04 +0500

    added blah blah blah

--- backend/settings.py
+++ backend/settings.py
@@ -14,6 +14,9 @@ from django.conf import settings
 from datetime import timedelta
 from pathlib import Path
 
+
+# blah blah blah     
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
